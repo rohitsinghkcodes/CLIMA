dc561ad141d98ffa20c3d7ef9212c25b74a185b9
Rohit Singh <conankmails@gmail.com>
2020-09-08 01:38:45 +0530

    working with weather json file

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
@@ -25,7 +27,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
     http.Response response = await http.get(
         'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
     if (response.statusCode == 200) {
-      print(response.body);
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+      var temp = decodedData['main']['temp'];
+      var condId = decodedData['weather'][0]['id'];
+      var city = decodedData['name'];
+      print(temp);
+      print(condId);
+      print(city);
     } else {
       print(response.statusCode);
     }
